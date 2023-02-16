0fafce2ba08dc0bac9ba7552ee8b39189ed4c293
ishimwepaul7 <ishimwepaul7@gmail.com>
2023-02-16 15:38:41 +0000

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with put function
+n
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
