5ba6dc95d789c45b894904337ce8f6200332f1ad
ishimwepaul7 <ishimwepaul7@gmail.com>
2023-02-16 15:48:54 +0000

    kjhs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * Return: Always 0.
  */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
