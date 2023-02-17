913edf1d5dfe91167122b736a63036398074e1b5
ishimwepaul7 <ishimwepaul7@gmail.com>
2023-02-17 00:11:14 +0000

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 		return (1);
 }
