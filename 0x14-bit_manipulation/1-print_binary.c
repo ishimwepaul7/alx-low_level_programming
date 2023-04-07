1f21a045c40282ea920b3acff37f64f8fc9c3f6f
ishimwepaul7 <ishimwepaul7@gmail.com>
2023-04-07 15:38:51 +0000

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
-
-	for (i = 63; i >= 0; i--)
-	{
-		current = n >> i;
-
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
-		_putchar('0')
+	if (n > 1)
+		printf_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
